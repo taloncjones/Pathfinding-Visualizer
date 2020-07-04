1573ad61b559cb30fddca2e67ec8b69ed1770067
Talon <taloncjones@gmail.com>
2020-07-03 19:21:42 -0700

    Add bottom frame

--- Gui/gui.py
+++ Gui/gui.py
@@ -3,34 +3,20 @@ import tkinter as tk
 
 LOGGER = logging.getLogger(__name__)
 
-DEFAULT_WH = 900
+DEFAULT_WH = 800
+TOPPANEL_H = 100
 
 class GUI:
     def __init__(self, rows, cols):
         self.draw = True
+        self.start = False
+        self.end = False
         self.rows = rows
         self.cols = cols
         self.grid = [[None for _ in range(self.cols)]
                      for _ in range(self.rows)]
         LOGGER.debug(
             'Grid created. Rows: {r}\tCols: {c}'.format(r=rows, c=cols))
-
-    # Create canvas with click and click-drag events and stores as self.c
-    def create_canvas(self):
-        self.c = tk.Canvas(self.window, width=DEFAULT_WH, height=DEFAULT_WH,
-                           borderwidth=5, background='grey')
-        self.c.pack()
-        self.c.bind('<ButtonPress-1>', self.set_draw)
-        self.c.bind('<B1-Motion>', self.callback)
-
-    # Inverts grid[row][col] at x, y and stores in self.draw. Then calls self.callback for click and drag drawing
-    def set_draw(self, event):
-        row, col = self.get_rc(event.x, event.y)
-        if not self.grid[row][col]:
-            self.draw = True
-        else:
-            self.draw = False
-        self.callback(event)
 
     # Creates window with default values and stores reference
     def create_window(self):
@@ -39,12 +25,16 @@ class GUI:
         root.config(background = '#FFFFFF')
         self.window = root
 
-    # Creates the top panel within the window
-    def top_panel(self):
-        self.topFrame = tk.Frame(self.window, width=DEFAULT_WH, height=100)
-        self.topFrame.grid(row=0, column=0, padx=10, pady=2)
-        tk.Label(self.topFrame, text='Controls:').grid(row=0, column=0, padx=10, pady=2)
-        btnFrame = tk.Frame(self.topFrame, width=DEFAULT_WH, height=200)
+    # Create canvas with click and click-drag events and stores as self.c
+    def create_canvas(self, frame, row=0, col=0):
+        self.c = tk.Canvas(self.window, width=DEFAULT_WH, height=DEFAULT_WH,
+                           borderwidth=5, background='grey')
+        self.c.grid(row=row, column=col)
+        self.c.bind('<ButtonPress-1>', self.set_draw)
+        self.c.bind('<B1-Motion>', self.callback)
+
+    def create_buttons(self, frame):
+        btnFrame = tk.Frame(frame, width=DEFAULT_WH, height=200)
         btnFrame.grid(row=1, column=0, padx=10, pady=2)
         startBtn = tk.Button(btnFrame, text='Start', command=None)
         startBtn.grid(row=1, column=0, padx=10, pady=2)
@@ -55,9 +45,22 @@ class GUI:
         goBtn = tk.Button(btnFrame, text='GO!', command=None)
         goBtn.grid(row=1, column=3, padx=10, pady=2)
 
+    # Creates the top panel within the window
+    def top_panel(self):
+        self.topFrame = tk.Frame(self.window, width=DEFAULT_WH, height=TOPPANEL_H)
+        self.topFrame.grid(row=0, column=0)
+        tk.Label(self.topFrame, text='Controls:').grid(row=0, column=0, padx=10, pady=2)
+        self.create_buttons(self.topFrame)
+
+    def bottom_panel(self):
+        self.btmFrame = tk.Frame(self.window, width=DEFAULT_WH, height=(DEFAULT_WH-TOPPANEL_H))
+        self.btmFrame.grid(row=1, column=0)
+        self.create_canvas(self.btmFrame, row=1)
+
     def render(self):
         self.create_window()
         self.top_panel()
+        self.bottom_panel()
         # self.create_canvas()
         LOGGER.debug('Launching...')
         self.window.mainloop()
@@ -80,6 +83,15 @@ class GUI:
             (col+width)*c_width,
             (row+width)*r_height,
             fill=color)
+
+    # Inverts grid[row][col] at x, y and stores in self.draw. Then calls self.callback for click and drag drawing
+    def set_draw(self, event):
+        row, col = self.get_rc(event.x, event.y)
+        if not self.grid[row][col]:
+            self.draw = True
+        else:
+            self.draw = False
+        self.callback(event)
 
     # Draws/erases rectangles at given x, y coordinates when called as event
     def callback(self, event):
